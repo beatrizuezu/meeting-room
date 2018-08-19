8dcb2f308e9cfcf7db31906d51c322aa988f3d5d
Beatriz Uezu <beatriz.uezu@gmail.com>
2018-08-18 22:20:17 -0300

    add versions for endopoints

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,11 @@
+from django.conf.urls import include
 from django.contrib import admin
 from django.urls import path
+
+from core.api import urls as api_urls
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('v1/', include(api_urls))
 ]
