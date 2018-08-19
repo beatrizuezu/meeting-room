211acf65b72dab79ed781b43cf12f5f8a59eab30
Beatriz Uezu <beatriz.uezu@gmail.com>
2018-08-18 22:18:52 -0300

    create str function for Room

--- core/meetings/models.py
+++ core/meetings/models.py
@@ -4,3 +4,6 @@ from django.db import models
 class Room(models.Model):
     name = models.CharField('name', max_length=50)
     is_available = models.BooleanField()
+
+    def __str__(self):
+        return '{}'.format(self.name)
