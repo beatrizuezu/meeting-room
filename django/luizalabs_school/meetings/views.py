7edf5941c4c7515ccd9b23c2d79ce145ed6c9227
Beatriz Uezu <beatriz.uezu@gmail.com>
2018-09-23 19:27:03 -0300

    create viewset for meeting

--- django/luizalabs_school/meetings/views.py
+++ django/luizalabs_school/meetings/views.py
@@ -1,8 +1,16 @@
 from rest_framework.viewsets import ModelViewSet
-from luizalabs_school.meetings.serializers import RoomSerializer
-from luizalabs_school.meetings.models import Room
+from luizalabs_school.meetings.serializers import (
+    MeetingSerializer,
+    RoomSerializer
+)
+from luizalabs_school.meetings.models import Meeting, Room
 
 
 class RoomViewSet(ModelViewSet):
     queryset = Room.objects.all()
     serializer_class = RoomSerializer
+
+
+class MeetingViewSet(ModelViewSet):
+    queryset = Meeting.objects.all()
+    serializer_class = MeetingSerializer
