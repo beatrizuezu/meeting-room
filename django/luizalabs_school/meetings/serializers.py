f132ec7e55df1304d400a17f643177fd0995b728
Beatriz Uezu <beatriz.uezu@gmail.com>
2018-09-23 19:28:41 -0300

    create MeetingSerializer

--- django/luizalabs_school/meetings/serializers.py
+++ django/luizalabs_school/meetings/serializers.py
@@ -1,10 +1,19 @@
 from rest_framework import serializers
 
-from luizalabs_school.meetings.models import Room
+from luizalabs_school.meetings.models import Meeting, Room
 
 
 class RoomSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Room
+        fields = '__all__'
+
+
+class MeetingSerializer(serializers.ModelSerializer):
+    room = serializers.PrimaryKeyRelatedField(
+        queryset=Room.objects.filter(is_available=True)
+    )
+    class Meta:
+        model = Meeting
         fields = '__all__'
