c24a7afb657b0c27647013f45832426e16839efd
Beatriz Uezu <beatriz.uezu@gmail.com>
2018-09-23 19:26:34 -0300

    create router for meeting

--- django/luizalabs_school/meetings/urls.py
+++ django/luizalabs_school/meetings/urls.py
@@ -2,11 +2,12 @@ from rest_framework import routers
 from django.conf.urls import include
 from django.urls import path
 
-from luizalabs_school.meetings.views import RoomViewSet
+from luizalabs_school.meetings.views import MeetingViewSet, RoomViewSet
 
 
 router = routers.DefaultRouter()
 router.register(r'rooms', RoomViewSet)
+router.register(r'meetings', MeetingViewSet)
 
 urlpatterns = [
     path('', include(router.urls))
