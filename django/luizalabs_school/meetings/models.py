d1ddf2ba32945b67e6f31bcc1e97f4ffefb9bb35
Beatriz Uezu <beatriz.uezu@gmail.com>
2018-09-22 21:46:46 -0300

    create Meeting

--- django/luizalabs_school/meetings/models.py
+++ django/luizalabs_school/meetings/models.py
@@ -7,3 +7,13 @@ class Room(models.Model):
 
     def __str__(self):
         return '{}'.format(self.name)
+
+
+class Meeting(models.Model):
+    description = models.CharField('description', max_length=100)
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
+    start_at = models.DateTimeField('start_at')
+    end_at = models.DateTimeField('end_at')
+
+    def __str__(self):
+        return '{}'.format(self.description)
