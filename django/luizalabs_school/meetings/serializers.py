f97a98f35a73364cf11df700485a9018782866fa
Beatriz Uezu <beatriz.uezu@gmail.com>
2018-10-07 23:36:03 -0300

    add validate for not schedule meeting on rooms in use

--- django/luizalabs_school/meetings/serializers.py
+++ django/luizalabs_school/meetings/serializers.py
@@ -1,3 +1,4 @@
+from django.db.models import Q
 from rest_framework import serializers
 
 from luizalabs_school.meetings.models import Meeting, Room
@@ -12,8 +13,21 @@ class RoomSerializer(serializers.ModelSerializer):
 
 class MeetingSerializer(serializers.ModelSerializer):
     room = serializers.PrimaryKeyRelatedField(
-        queryset=Room.objects.filter(is_available=True)
+        queryset=Room.objects.all()
     )
+
+    def validate(self, data):
+        meeting = Meeting.objects.filter(
+            Q(room=data['room']),
+            Q(start_at__gte=data['start_at']) | Q(end_at__lte=data['start_at']),
+            Q(start_at__gte=data['end_at']) | Q(end_at__lte=data['end_at'])
+        )
+
+        if meeting:
+            raise serializers.ValidationError("room occupied")
+
+        return data
+
     class Meta:
         model = Meeting
         fields = '__all__'
